8d5a1f97ba4bb92f99f355984d44915e61b01ceb
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-12 23:11:33 +0530

    Update Que5.java

--- src/task10/Que5.java
+++ src/task10/Que5.java
@@ -1,39 +1,43 @@
 package task10;
 
- class Tea { // create Tea class 
+class Tea { // create Tea class 
 	 
-    public boolean milk;
-    public boolean sugar;
+public boolean milk;
+public boolean sugar;
 
-    public Tea() {
+public Tea() {
         this.milk = false;
         this.sugar = false;
-    }
+}
 
-    public void prepareTea() {
+public void prepareTea() {
         System.out.println("Preparing tea with hot water and tea leaves.");
-    }
+}
 
-    public void addMilk() {
+public void addMilk() {
         this.milk = true;
         System.out.println("Adding milk to the tea.");
-    }
+}
 
-    public void addSugar() {
+public void addSugar() {
         this.sugar = true;
         System.out.println("Adding sugar to the tea.");
-    }
+}
 }
 public class Que5 {
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Tea tea = new Tea();
+	Tea tea = new Tea();
 		
-		tea.prepareTea(); // Prepare the basic tea 
-		tea.addMilk(); // Add milk to the basic tea 
-		tea.addSugar(); // Add sugar to the basic tea 
-	}
-
+	tea.prepareTea(); // Prepare the basic tea 
+	tea.addMilk(); // Add milk to the basic tea 
+	tea.addSugar(); // Add sugar to the basic tea 
+    }
 }
+----------------------------------------------------------------------------------------------------------------
+OUTPUT:
+
+Preparing tea with hot water and tea leaves.
+Adding milk to the tea.
+Adding sugar to the tea.
