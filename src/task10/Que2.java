56e26dfc10c52553e4b7d312ecd89594b61f8c93
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-12 22:59:59 +0530

    Update Que2.java

--- src/task10/Que2.java
+++ src/task10/Que2.java
@@ -1,45 +1,47 @@
 package task10;
 
- class Employee {  // implement Employee class 
-    public int id; 
-    public String name; 
-    public double salary; // Declaring attributes
+class Employee {  // implement Employee class 
+public int id; 
+public String name; 
+public double salary; // Declaring attributes
  
-    public Employee(int id, String name, double salary) { 
-        this.id = id; 
+public Employee(int id, String name, double salary) { 
+	this.id = id; 
         this.name = name; 
         this.salary = salary; 
-    } 
- 
-    public void raiseSalary(double percent) { // Method()
-        salary += salary * percent / 100; 
-    } 
- 
-    public int getId() { 
-        return id; 
-    } 
- 
-    public String getName() { 
-        return name; 
-    } 
- 
-    public double getSalary() { 
-        return salary; 
-    } 
 } 
-
+ 
+public void raiseSalary(double percent) { // Method()
+        salary += salary * percent / 100; 
+} 
+ 
+public int getId() { 
+        return id; 
+} 
+ 
+public String getName() { 
+        return name; 
+} 
+ 
+public double getSalary() { 
+        return salary; 
+} 
+} 
 
 public class Que2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Employee employee = new Employee(714, "Michel", 45000); 
-		System.out.println("Before raise: " + employee.getSalary()); 
+	Employee employee = new Employee(714, "Michel", 45000); 
+	System.out.println("Before raise: " + employee.getSalary()); 
 		
-		employee.raiseSalary(10); 
-		System.out.println("After raise: " + employee.getSalary()); 
-
-	}
-
+	employee.raiseSalary(10); 
+	System.out.println("After raise: " + employee.getSalary()); 
+     }
 }
+-------------------------------------------------------------------------------------------------------------
+OUTPUT:
+	
+Before raise: 45000.0
+After raise: 49500.0
